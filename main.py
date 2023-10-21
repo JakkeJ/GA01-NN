467e51abd6f789959640a847b36e7ea6bface175
JakkeJ <jimtore2k@gmail.com>
2023-10-21 12:03:47 +0200

    Only using parallel if theres more than one GPU

--- main.py
+++ main.py
@@ -152,7 +152,7 @@ def main(args):
     else:
         print(f'CUDA-devices: {", ".join(map(str, devices))}')
 
-    if device == torch.device('cuda'):
+    if device == torch.device('cuda') and len(devices) > 1:
         model = torch.nn.parallel.DataParallel(model, device_ids = devices)
     else:
         model.to(device)
